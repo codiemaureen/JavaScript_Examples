cdf0ca4443b4b2f50b563ca93eef37e377ebabfd
Codie <77300264+codiemaureen@users.noreply.github.com>
2022-02-03 13:02:57 -0500

    Sizing up the page

--- Mouse Move Shadow/scripts.js
+++ Mouse Move Shadow/scripts.js
@@ -1,12 +1,20 @@
 const hero = document.querySelector('.hero');
 const text = hero.querySelector('h1');
+const walk = 100;
 
 
 function shadow (e){
     //get height and width of the screen
     const { offsetWidth: width, offsetHeight: height } = hero;
     let  { offsetX: x, offsetY: y} = e;
-    console.log(x,y);
+    
+    if(this != e){
+        x = x + e.target.offsetLeft;
+        y= y + e.target.offsetTop;
+    };
+    const xWalk = Math.round((x / width * walk ) - (walk / 2));
+    const yWalk = Math.round((y / width * walk ) - (walk / 2));
+    console.log(xWalk, yWalk);
 };
 
 hero.addEventListener('mousemove', shadow);
