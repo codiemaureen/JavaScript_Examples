a3a286affd5cc41e2a43b03a8da3e219ae82e15d
Codie <77300264+codiemaureen@users.noreply.github.com>
2022-02-24 17:04:06 -0500

    Getting Options

--- Speech Synthesis/scripts.js
+++ Speech Synthesis/scripts.js
@@ -20,10 +20,17 @@ function setVoice(){
     toggle();
 }
 
-function toggle(){
+function toggle(startOver = true){
     speechSynthesis.cancel();
-    speechSynthesis.speak(msg);
+    if(startOver){
+        speechSynthesis.speak(msg);
+    };
+};
+
+function setOption(){
+    console.log(this.name, this.value);
 }
 
 speechSynthesis.addEventListener('voiceschanged', populateVoices);
 voicesDropdown.addEventListener('change', setVoice);
+options.forEach(option => option.addEventListener('change', setOption));
