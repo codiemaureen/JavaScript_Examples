e170751f352900ef7a7ee66a2a8fb6fa656156bc
Codie <77300264+codiemaureen@users.noreply.github.com>
2022-02-24 16:13:28 -0500

    Gatting List of Voices

--- Speech Synthesis/scripts.js
+++ Speech Synthesis/scripts.js
@@ -4,3 +4,13 @@ const voicesDropdown = document.querySelector('[name="voice"]');
 const options = document.querySelectorAll('[type="range"], [name="text"]');
 const speakButton = document.querySelector('#speak');
 const stopButton = document.querySelector('#stop');
+
+msg.text = document.querySelector('[name="text"]').nodeValue;
+
+
+function populateVoices(){
+    voices = this.getVoices();
+    console.log(voices);
+}
+
+speechSynthesis.addEventListener('voiceschanged', populateVoices);
