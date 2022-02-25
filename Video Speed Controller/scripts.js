9c136bca50586321e3471e4f9ae1eed6886c57b4
Codie <77300264+codiemaureen@users.noreply.github.com>
2022-02-25 14:19:49 -0500

    Adding bar style

--- Video Speed Controller/scripts.js
+++ Video Speed Controller/scripts.js
@@ -5,5 +5,9 @@ const video = document.querySelector('.flex');
 
 speed.addEventListener('mousemove', function(e){
     const y = e.pageY - this.offsetTop;
-    console.log(y);
+    const percent = y/ this.offsetHeight;
+    const min = 0.4;
+    const max = 4;
+    const height = Math.round(percent * 100) + '%';
+    bar.style.height = height;
 });
