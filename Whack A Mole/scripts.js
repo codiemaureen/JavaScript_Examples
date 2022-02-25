2a14564edfe86d982e8df7b1e23b5c01ef8d1726
Codie <77300264+codiemaureen@users.noreply.github.com>
2022-02-25 15:35:37 -0500

    Get random time

--- Whack A Mole/scripts.js
+++ Whack A Mole/scripts.js
@@ -1,3 +1,8 @@
 const holes = document.querySelectorAll('.hole');
 const scoreBoard = document.querySelector('.score');
 const moles = document.querySelectorAll('.mole');
+
+
+function randTime(min, max){
+    return Math.round(Math.random() * (max - min) + min);
+};
