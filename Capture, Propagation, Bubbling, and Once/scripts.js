44df0e28d70a1e940957f358c362837e1ca91e90
Codie <77300264+codiemaureen@users.noreply.github.com>
2022-02-25 10:55:42 -0500

    Capture

--- Capture, Propagation, Bubbling, and Once/scripts.js
+++ Capture, Propagation, Bubbling, and Once/scripts.js
@@ -1,8 +1,10 @@
 const divs = document.querySelectorAll('div');
 
 function showText(e){
-    e.stopPropagation(); //stop bubbling
+    // e.stopPropagation(); //stop bubbling
     console.log(this);
 }
 
-divs.forEach(div => div.addEventListener('click', showText));
+divs.forEach(div => div.addEventListener('click', showText, {
+    capture: true, //start at the top
+}));
