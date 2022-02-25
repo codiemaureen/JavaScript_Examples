ea231671b796b0303fa4eb344acf3f4078b6a1e2
Codie <77300264+codiemaureen@users.noreply.github.com>
2022-02-25 15:49:35 -0500

    Adding Start Game Func

--- Whack A Mole/scripts.js
+++ Whack A Mole/scripts.js
@@ -2,6 +2,7 @@ const holes = document.querySelectorAll('.hole');
 const scoreBoard = document.querySelector('.score');
 const moles = document.querySelectorAll('.mole');
 let lastHole;
+let timeUp = false;
 
 function randomTime(min, max){
     return Math.round(Math.random() * (max - min) + min);
@@ -26,5 +27,14 @@ function peep(){
     hole.classList.add('up')
     setTimeout(() => {
         hole.classList.remove('up');
+        if(!timeUp) peep();
     }, time);
+};
+
+
+function startGame(){
+    scoreBoard.textContent = 0;
+    timeUp = false;
+    peep();
+    setTimeout(() => timeUp = true, 10000);
 }
