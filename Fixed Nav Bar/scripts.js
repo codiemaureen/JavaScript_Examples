f9d2953548bc704b5ad7d093e9ebcd0eaa2a7861
Codie <77300264+codiemaureen@users.noreply.github.com>
2022-02-25 10:15:08 -0500

    Fixing nav to top

--- Fixed Nav Bar/scripts.js
+++ Fixed Nav Bar/scripts.js
@@ -3,7 +3,11 @@ const topOfNav = nav.offsetTop;
 
 
 function fixNav () {
-    console.log(topOfNav, window.scrollY);
+    if(window.scrollY >= topOfNav) {
+        document.body.classList.add('fixed-nav');
+    } else {
+        document.body.classList.remove('fixed-nav');
+    }
 };
 
 window.addEventListener('scroll', fixNav);
