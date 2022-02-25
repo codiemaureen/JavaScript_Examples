22b17c56d72e6cb4e34f10dd379790cbb5e1881a
Codie <77300264+codiemaureen@users.noreply.github.com>
2022-02-25 15:55:18 -0500

    Adding Scoreboad

--- Whack A Mole/scripts.js
+++ Whack A Mole/scripts.js
@@ -3,6 +3,7 @@ const scoreBoard = document.querySelector('.score');
 const moles = document.querySelectorAll('.mole');
 let lastHole;
 let timeUp = false;
+let score = 0;
 
 function randomTime(min, max){
     return Math.round(Math.random() * (max - min) + min);
@@ -37,4 +38,13 @@ function startGame(){
     timeUp = false;
     peep();
     setTimeout(() => timeUp = true, 10000);
-}
+};
+
+function bonk(e){
+    if(!e.isTrusted) return;
+    score++;
+    this.classList.remove('up');
+    scoreBoard.textContent = score;
+};
+
+moles.forEach(mole => mole.addEventListener('click', bonk));
