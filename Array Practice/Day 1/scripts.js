a74c95dfbe1ae9f21c5121c4d0cddcdcbf35823d
Codie <77300264+codiemaureen@users.noreply.github.com>
2022-02-28 16:46:25 -0500

    Map and filter

--- Array Practice/Day 1/scripts.js
+++ Array Practice/Day 1/scripts.js
@@ -73,13 +73,13 @@
     // const category = document.querySelector('.mw-category');
     // const links = [...category.querySelectorAll('a')];
 
-    
-
+    // const de = links
+    //     .map(link => link.textContent)
+    //     .filter(streetName => streetName.includes('de'));
 
 
     // 7. sort Exercise
     // Sort the people alphabetically by last name
-
 
 
     
