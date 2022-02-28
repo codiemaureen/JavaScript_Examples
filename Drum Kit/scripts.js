7d24ae35e51600b8da3c4c7b53dab93b42e8bb98
Codie <77300264+codiemaureen@users.noreply.github.com>
2022-02-28 13:42:40 -0500

    Animating Keys

--- Drum Kit/scripts.js
+++ Drum Kit/scripts.js
@@ -1,5 +1,8 @@
 window.addEventListener('keydown', function(e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   if(!audio) return; // stop fn from running
+  audio.currentTime = 0; //allow sound to replay
   audio.play();
+
+  
 });
