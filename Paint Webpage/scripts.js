1dbc6f6b88b2cf657260cd5f9ed722c7ac1affe3
Codie <77300264+codiemaureen@users.noreply.github.com>
2022-02-28 14:55:09 -0500

    Changing line size

--- Paint Webpage/scripts.js
+++ Paint Webpage/scripts.js
@@ -14,6 +14,7 @@ let isDrawing = false;
 let lastX = 0 ;
 let lastY = 0;
 let hue = 150;
+let direction = true;
 
 function draw (e){
   if(!isDrawing) return;
@@ -25,7 +26,21 @@ function draw (e){
   ctx.lineTo(e.offsetX, e.offsetY);
   ctx.stroke();
   [lastX, lastY] = [e.offsetX, e.offsetY];
+  
   hue++
+  if(hue > 360){
+    hue = 0;
+  }
+
+  if(ctx.lineWidth >= 100 || ctx.lineWidth <= 1){
+    direction = !direction;
+  }
+
+  if(direction){
+    ctx.lineWidth++
+  } else {
+    ctx.lineWidth--;
+  }
 }
 
 canvas.addEventListener('mousemove', draw);
