b78ff795cf39af71e55f8e60904cec68604a9a6a
Codie <77300264+codiemaureen@users.noreply.github.com>
2022-02-28 17:19:07 -0500

    Initial Setup

---
+++ Dev Tools Tips/scripts.js
@@ -0,0 +1,52 @@
+    //attribute modifications
+
+
+    //click to change color
+
+
+    // Regular
+    console.log('Hello!');
+
+
+    // Interpolated
+
+
+
+    // Styled
+
+
+
+    // warning!
+
+
+
+
+    // Error :|
+
+
+    // Info
+
+
+
+    // Testing
+
+  
+
+
+    // clearing
+
+
+
+
+
+    // Grouping together
+
+
+
+    // counting
+
+
+
+    // timing
+
+  
