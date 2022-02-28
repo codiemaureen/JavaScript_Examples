edc8726aa680ab6b28f22c1989e06627e401ca00
Codie <77300264+codiemaureen@users.noreply.github.com>
2022-02-28 15:16:07 -0500

    Copying Arrays

--- JavaScript References VS Copying/scripts.js
+++ JavaScript References VS Copying/scripts.js
@@ -1,1 +1,30 @@
-  
+const players = ['Jane', 'Tom', 'Jack', 'Carrie'];
+
+//copy the players
+
+const team = players;
+console.log(team);
+
+
+//updates both original and copy
+team[3] = 'Rosie';
+console.log(team, players);
+
+//slice allows you have to an changable copy
+const team2 = players.slice();
+team2[3] = 'Carrie';
+console.log(team2);
+
+//you can also concatinate (works the same as slice)
+const team3 = [].concat(team2);
+console.log(team3);
+
+
+//copy with spread
+const team4 = [...players];
+team4[4] = 'Rosie';
+console.log(team4);
+
+//copy with Array.from()
+const team5 = Array.from(team4);
+console.log(players);
