ad29ab2be1c65dd3f53e3b63b94a38e48c3a7cc3
Codie <77300264+codiemaureen@users.noreply.github.com>
2022-03-01 10:35:08 -0500

    Getting in between inputs

--- Check Multi Boxes with Shift/scripts.js
+++ Check Multi Boxes with Shift/scripts.js
@@ -1,8 +1,19 @@
 const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
 
+let lastChecked;
 
 function handleClick(e){
-  console.log(e);
+  //check for shift key
+  let inBetween = false;
+  if(e.shiftKey && this.checked){
+    checkboxes.forEach(checkbox => {
+      if(checkbox === this || checkbox === lastChecked){
+        inBetween = !inBetween;
+        console.log(checkbox);
+      }
+    });
+  };
+  lastChecked = this;
 }
 
 checkboxes.forEach(checkbox => checkbox.addEventListener('click', handleClick));
